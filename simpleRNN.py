9678b30063c79a4bb6ad3bebdc4f56ec8ab85b58
biprajiman <manish.sapkota@gmail.com>
2017-03-06 14:19:01 -0500

    done with the RNN model

--- simpleRNN.py
+++ simpleRNN.py
@@ -58,17 +58,23 @@ class SRNN:
 
     SRNN.predict = predict
 
+    '''
+    Total of the loss for the given samples
+    I am assuming these will be batches
+    '''
     def calculate_sum_loss(self, x, y):
         L = 0
         # loop through each of the sentences
-        for i in np.arange(len(y)):
+        for i in np.arange(len(y)): # check the dimension for x and y
             o, s = self.fp(x[i])
-            correct_word_predictions =  o[np.arange(len(y[i])), y[i]]
+            correct_word_predictions =  o[np.arange(len(y[i])), y[i]] # not sure what this line does
             # Add the loss
             L+=-1 * np.sum(np.log(correct_word_predictions))
 
         return L
-    
+    '''
+    Compute the normalized loss
+    '''
     def calculate_loss(self, x, y):
         N = np.sum((len(y_i) for y_i in y))
         return self.calculate_sum_loss(x,y)/N
@@ -76,5 +82,71 @@ class SRNN:
     SRNN.calculate_sum_loss = calculate_sum_loss
     SRNN.calculate_loss = calculate_loss
 
+    def bptt(self, x, y):
+        T = len(y)
+        o, s = self.fp(x)
+
+        # gradient of the parameters
+        dLdU = np.zeros(self.U.shape)
+        dLdW = np.zeros(self.W.shape)
+        dLdV = np.zeros(self.V.shape)
+
+        delta_o[np.arrange(len(y)), y]  -= 1
+
+        for t in np.arange(T) [::-1]:
+            dLdV += np.outer(delta_o[t], s[t].T)
+            delta_t =  self.V.T.dot(delta_o[t]) * (1 - (s[t] ** 2))
+
+            for bptt_step in np.arange(max(0, t-self.bptt_steps), t+1)[::-1]:
+                dLdW += np.outer(delta_t, s[bptt_step - 1])
+                dLdU[:, x[bptt_step]] += delta_t
+                delta_t =  self.W.T.dot(delta_t) * (1 - s[bptt_step - 1] ** 2)
+        return [dLdU, dLdV, dLdW]
     
+    SRNN.bptt = bptt
+
+
+    def gradient_check(self, x, y, h=0.001, error_threshold = 0.01):
+        # Calculate the gradients using backpropagation. We want to checker if these are correct.
+        bptt_gradients = self.bptt(x, y)
+        # List of all parameters we want to check.
+        model_parameters = ['U', 'V', 'W']
+        # Gradient check for each parameter
+        for pidx, pname in enumerate(model_parameters):
+            # Get the actual parameter value from the mode, e.g. model.W
+            parameter = operator.attrgetter(pname)(self)
+            print "Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape))
+            # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
+            it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
+            while not it.finished:
+                ix = it.multi_index
+                # Save the original value so we can reset it later
+                original_value = parameter[ix]
+                # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
+                parameter[ix] = original_value + h
+                gradplus = self.calculate_total_loss([x],[y])
+                parameter[ix] = original_value - h
+                gradminus = self.calculate_total_loss([x],[y])
+                estimated_gradient = (gradplus - gradminus)/(2*h)
+                # Reset parameter to original value
+                parameter[ix] = original_value
+                # The gradient for this parameter calculated using backpropagation
+                backprop_gradient = bptt_gradients[pidx][ix]
+                # calculate The relative error: (|x - y|/(|x| + |y|))
+                relative_error = np.abs(backprop_gradient - estimated_gradient)/(np.abs(backprop_gradient) + np.abs(estimated_gradient))
+                # If the error is to large fail the gradient check
+                if relative_error > error_threshold:
+                    print "Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix)
+                    print "+h Loss: %f" % gradplus
+                    print "-h Loss: %f" % gradminus
+                    print "Estimated_gradient: %f" % estimated_gradient
+                    print "Backpropagation gradient: %f" % backprop_gradient
+                    print "Relative Error: %f" % relative_error
+                    return
+                it.iternext()
+
+            print "Gradient check for parameter %s passed." % (pname)
+ 
+RNNNumpy.gradient_check = gradient_check
+
 
