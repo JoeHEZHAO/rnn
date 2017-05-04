470f0f3a708e00c8d0202cc2e5237110d0dd5323
Manish Sapkota <manish@Manishs-MacBook-Pro.local>
2017-05-04 18:33:50 -0400

    scratching head

--- rnn_tensorflow.py
+++ rnn_tensorflow.py
@@ -38,158 +38,24 @@ class TenRNN(object):
         
     def __tensorflow_build__(self):
         U, V, W = self.U, self.V, self.W
-        x = 
+        
+        # inputs
+        xs_ = tf.placeholder(shape=[None, None], 
+                             dtype=tf.int32)
+        ys_ = tf.placeholder(shape=[None], 
+                             dtype=tf.int32)
 
-        def step(st_1, xt):
+        initial_state = tf.placeholder(shape=[None, self.hidden_dim], 
+                                       dtype=tf.float32, 
+                                       name='initial_state')
+        
+        def step(st_prev, xt):
+            return tf.matmul(st_prev, W) + 
+                   tf.matmul(x, U)
+        
 
-    '''
-    Forward propagation
-    x : sentence with each word considered as a time step
-    TODO: consider updating for batch
-    '''
-    def fp(self,x):
-        # total number of time steps
-        T = len(x) # length of the sentence is the time steps
-
-        # save all the previous states since we need it for the computation
-        s = np.zeros((T+1, self.hidden_dim))
-        s[-1] = np.zeros(self.hidden_dim)
-
-        # The outputs at each time step. Save them for later as well
-        o = np.zeros((T, self.word_dim))
-
-        # for each time step. Each time step is a word
-        for t in np.arange(T):
-            s[t] = np.tanh(self.U[:,x[t]] +self.W.dot(s[t-1]))
-            o[t] = softmax(self.V.dot(s[t]))
-
-        return [o, s]
 
     
-    '''
-    Prediction
-    '''
-    def predict(self,x):
-        o, s = self.fp(x)
-        return np.argmax(o, axis=1) # returns the index of maximum apply accross the column
-
-    #SRNN.predict = predict
-
-    '''
-    Total of the loss for the given samples
-    I am assuming these will be batches
-    '''
-    def calculate_sum_loss(self, x, y):
-        L = 0
-        # loop through each of the sentences
-        for i in np.arange(len(y)): # check the dimension for x and y
-            o, s = self.fp(x[i])
-            correct_word_predictions = o[np.arange(len(y[i])), y[i]] # probability of each predicted word
-            # Add the loss
-            L += -1 * np.sum(np.log(correct_word_predictions))
-
-        return L
-    '''
-    Compute the normalized loss
-    N here is the total words/time steps for a sentence
-    '''
-    def calculate_loss(self, x, y):
-        N = np.sum((len(y_i) for y_i in y))
-        return self.calculate_sum_loss(x, y)/N
-
-    #SRNN.calculate_sum_loss = calculate_sum_loss
-    #SRNN.calculate_loss = calculate_loss
-
-    '''
-    Do the mathematics for the back-propagation should be straight forward
-    '''
-    def bptt(self, x, y):
-        T = len(y)
-        o, s = self.fp(x)
-
-        # gradient of the parameters
-        dLdU = np.zeros(self.U.shape)
-        dLdW = np.zeros(self.W.shape)
-        dLdV = np.zeros(self.V.shape)
-        delta_o = o
-        delta_o[np.arange(len(y)), y] -= 1
-
-        for t in np.arange(T) [::-1]:
-            dLdV += np.outer(delta_o[t], s[t].T)
-            delta_t = self.V.T.dot(delta_o[t]) * (1 - (s[t] ** 2))
-
-            for bptt_step in np.arange(max(0, t-self.bptt_steps), t+1)[::-1]: # similar to for i in range(100, -1, -1)
-                dLdW += np.outer(delta_t, s[bptt_step - 1])
-                dLdU[:, x[bptt_step]] += delta_t
-                delta_t =  self.W.T.dot(delta_t) * (1 - s[bptt_step - 1] ** 2)
-        return [dLdU, dLdV, dLdW]
-    
-    #SRNN.bptt = bptt
-
-    '''
-    Dig into what is going on in the gradient_check
-    This is important
-    '''
-    def gradient_check(self, x, y, h=0.001, error_threshold=0.01):
-        # Calculate the gradients using backpropagation. We want to checker if these are correct.
-        bptt_gradients = self.bptt(x, y)
-        # List of all parameters we want to check.
-        model_parameters = ['U', 'V', 'W']
-        # Gradient check for each parameter
-        for pidx, pname in enumerate(model_parameters):
-            # Get the actual parameter value from the mode, e.g. model.W
-            parameter = operator.attrgetter(pname)(self)
-            print("Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape)))
-            # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
-            it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
-            while not it.finished:
-                ix = it.multi_index
-                # Save the original value so we can reset it later
-                original_value = parameter[ix]
-
-                # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
-                parameter[ix] = original_value + h
-                gradplus = self.calculate_total_loss([x], [y])
-                parameter[ix] = original_value - h
-                gradminus = self.calculate_total_loss([x], [y])
-                estimated_gradient = (gradplus - gradminus)/(2*h)
-
-                # Reset parameter to original value
-                parameter[ix] = original_value
-
-                # The gradient for this parameter calculated using backpropagation
-                backprop_gradient = bptt_gradients[pidx][ix]
-
-                # calculate The relative error: (|x - y|/(|x| + |y|))
-                relative_error = np.abs(backprop_gradient - estimated_gradient)/(np.abs(backprop_gradient) + np.abs(estimated_gradient))
-
-                # If the error is to large fail the gradient check
-                if relative_error > error_threshold:
-                    print ("Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix))
-                    print ("+h Loss: %f" % gradplus)
-                    print ("-h Loss: %f" % gradminus)
-                    print ("Estimated_gradient: %f" % estimated_gradient)
-                    print ("Backpropagation gradient: %f" % backprop_gradient)
-                    print ("Relative Error: %f" % relative_error)
-                    return
-                    
-                it.iternext()
-
-            print ("Gradient check for parameter %s passed." % (pname))
-
-    #SRNN.gradient_check = gradient_check
-
-    '''
-    Single stochastic gradient descent
-    '''
-    def sgd_step(self, x, y, learning_rate=0.1):
-        # get the gradients for the parameters
-        dLdU, dLdV, dLDw = self.bptt(x, y)
-
-        #update the parameters
-        self.U -= learning_rate * dLdU
-        self.V -= learning_rate * dLdV
-        self.W -= learning_rate * dLDw
 
 
 
